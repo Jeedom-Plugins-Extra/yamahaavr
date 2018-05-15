5ebb4b60a211fb2a3d79c5f1f17c179e72afd47e
ColonelMoutarde <4697568+ColonelMoutarde@users.noreply.github.com>
2018-05-15 10:11:06 +0200

    Licence and little improvement

--- core/class/yamahaavr.class.php
+++ core/class/yamahaavr.class.php
@@ -16,8 +16,23 @@
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* * ***************************Includes********************************* */
-require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+/* This file is part of NextDom.
+ *
+ * NextDom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NextDom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NextDom. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+require_once __DIR__ . '/../../../../core/php/core.inc.php';
 
 class yamahaavr extends eqLogic
 {
@@ -326,8 +341,7 @@ class yamahaavr extends eqLogic
         }
         $cmd = $this->getCmd(null, 'power_state');
         if (is_object($cmd) && isset($infos['ZonePower'])) {
-            $value = ($infos['ZonePower'] == 'OFF') ? 0 : 1;
-            $value = $cmd->formatValue($value);
+            $value = $cmd->formatValue(($infos['ZonePower'] == 'OFF') ? 0 : 1);
             if ($value != $cmd->execCmd(null, 2)) {
                 $cmd->setCollectDate('');
                 $cmd->event($value);
